d82e1f8dda69cab1faddc4c5b5fbfbde3fc40c04
jeoniltae <foodchain@naver.com>
2024-06-20 00:04:41 +0900

    6장: useState 및 useEffect훅 작성

---
+++ hook-test-app/src/App01.tsx
@@ -0,0 +1,21 @@
+import React, { ChangeEvent, useState } from "react";
+
+const App01 = () => {
+  const [msg, setMsg] = useState<string>("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={msg}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          setMsg(e.target.value);
+        }}
+      />
+      <br />
+      <span>입력 메시지: {msg}</span>
+    </div>
+  );
+};
+
+export default App01;
---
+++ hook-test-app/src/App02.tsx
@@ -0,0 +1,29 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
+
+// 컴포넌트가 마운트 및 업데이트될 때
+const App02 = () => {
+  const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("아이유");
+
+  useEffect(() => {
+    console.log(`${name} 님이 ${count}번 클릭했습니다`);
+  }, []);
+
+  return (
+    <div>
+      이름 변경:
+      <input
+        type="text"
+        value={name}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+      />
+      <hr />
+      <button onClick={() => setCount(count + 1)}>카운트 1증가</button>
+      <p>
+        {name} 님이 {count}번 클릭했습니다.
+      </p>
+    </div>
+  );
+};
+
+export default App02;
